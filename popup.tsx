f1c5973ef0a827b54f0ef90804d183fe55dcc219
paultreanor <treanorpaul9@gmail.com>
2024-09-17 21:01:37 +0100

    Remove everything that isn't a calendar

--- popup.tsx
+++ popup.tsx
@@ -1,25 +1,12 @@
-import { useState } from "react"
 import CalendarComponent from "./components/calendar"
 import 'react-calendar/dist/Calendar.css';
+import './styles/popup.css';
 
 function IndexPopup() {
 
   return (
-    <div
-      style={{
-        padding: 20,
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
+    <div className="container">
       <CalendarComponent />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
     </div>
   )
 }
