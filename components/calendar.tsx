a5cea6b7010161bc3376c68926b6edafdd157b53
paultreanor <treanorpaul9@gmail.com>
2024-09-17 21:30:16 +0100

    Replace react-calendar with mui date picker

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -1,19 +1,28 @@
-import Calendar from 'react-calendar';
+import { styled } from '@mui/material/styles';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
+
+const StyledDateCalendar = styled(DateCalendar)({
+  // Add any custom styles here
+});
 
 export default function CalendarComponent() {
-
-    const handleDateChange = (value: Date | Date[]) => {
-        const formattedDate = (value as Date).toISOString().split('T')[0].replace(/-/g, '/');
-        const calendarUrl = `https://calendar.google.com/calendar/u/0/r/week/${formattedDate}?pli=1`;
-        window.open(calendarUrl, '_blank');
+    const handleDateChange = (value: dayjs.Dayjs | null) => {
+        if (value) {
+            const formattedDate = value.format('YYYY/MM/DD');
+            const calendarUrl = `https://calendar.google.com/calendar/u/0/r/week/${formattedDate}?pli=1`;
+            window.open(calendarUrl, '_blank');
+        }
     };
 
     return (
-        <div>
-            <Calendar 
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StyledDateCalendar 
                 onChange={handleDateChange}
-                minDetail="year"
+                views={['day']}
             />
-        </div>
+        </LocalizationProvider>
     );
 }
