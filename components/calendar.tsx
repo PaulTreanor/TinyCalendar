62b2aaa65a360ff6d45fa466fa194b29e87d8e04
paultreanor <treanorpaul9@gmail.com>
2024-09-17 21:18:06 +0100

    Add google calendar opening functionality

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
 import Calendar from 'react-calendar';
 
 export default function CalendarComponent() {
-    const [date, setDate] = useState(new Date());
+
+    const handleDateChange = (value: Date | Date[]) => {
+        const formattedDate = (value as Date).toISOString().split('T')[0].replace(/-/g, '/');
+        const calendarUrl = `https://calendar.google.com/calendar/u/0/r/week/${formattedDate}?pli=1`;
+        window.open(calendarUrl, '_blank');
+    };
 
     return (
         <div>
             <Calendar 
-                onChange={(value) => setDate(value as Date)} 
-                value={date} 
+                onChange={handleDateChange}
                 minDetail="year"
             />
         </div>
